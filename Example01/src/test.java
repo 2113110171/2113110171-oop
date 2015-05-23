b92b567c9b0d0308a6572b16af7a775be9e85e6c
Admin <Admin@D20916>
2015-05-23 14:31:46 +0700

    nhu

---
+++ Example01/src/test.java
@@ -0,0 +1,15 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Rectangle recta =new Rectangle();
+Rectangle rectb =new Rectangle();
+	recta.setValues(3,4);
+	rectb.setValues(5,6);
+	 System.out.println("rect a:"+recta.area());
+	 System.out.println("rect b:"+rectb.area());
+
+	}
+
+}
